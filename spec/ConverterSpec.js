77725cf000541fa43f85f20e03587895943cd5b7
Matteo 'Peach' Pescarin <peach@smartart.it>
2013-10-19 19:13:02 +0100

    completed converter module
    - updated test with more convenient format
    - created specific matcher for lazy object comparison
    - created model and implemented needed functions

--- spec/ConverterSpec.js
+++ spec/ConverterSpec.js
@@ -7,15 +7,27 @@ describe('Converter', function () {
     it('should return the right calculated amount', function () {
         var values =   [ 100, 120, 350, 1000 ],
             expected = [
-                { '£1': 1 },
-                { '£1': 1, '20p': 1 },
-                { '£2': 1, '£1': 1, '50p': 1 },
-                { '£2': 5 }
+                [{ name: '\xA31', count: 1, value: 100 }],
+                [{ name: '\xA31', count: 1, value: 100 }, { name: '20p', count: 1, value: 20 }],
+                [{ name: '\xA32', count: 1, value: 200 }, { name: '\xA31', count: 1, value: 100 }, { name: '50p', count: 1, value: 50 }],
+                [{ name: '\xA32', count: 5, value: 200 }]
             ],
             i;
 
+        this.addMatchers({
+            toBeSimilar: function(expected) {
+                var similar = true;
+                for (field in expected) {
+                    if (expected.field !== this.actual.field) {
+                        similar = false;
+                    }
+                }
+                return similar;
+            }
+        });
+
         for (i in values) {
-            expect(Converter.convert(values[i])).toContain(expected[i]);
+            expect(Converter.convert(values[i])).toBeSimilar(expected[i]);
         }
     });
 });
--- src/converter.js
+++ src/converter.js
@@ -1,5 +1,99 @@
+PENNIES.namespace('Converter');
+
 /**
- * create or restore default namespace
- * @type {*|{}}
+ * Converter module
  */
-var PENNIES = PENNIES || {};
+PENNIES.Converter = (function () {
+    /**
+     * defines the list of monies available.
+     * @type {Array}
+     */
+    var moniesSizes = [
+        {
+            name: '\xA32',
+            value: 200,
+            count: 0
+        },
+        {
+            name: '\xA31',
+            value: 100,
+            count: 0
+        },
+        {
+            name: '50p',
+            value: 50,
+            count: 0
+        },
+        {
+            name: '20p',
+            value: 20,
+            count: 0
+        },
+        {
+            name: '10p',
+            value: 10,
+            count: 0
+        },
+        {
+            name: '5p',
+            value: 5,
+            count: 0
+        },
+        {
+            name: '2p',
+            value: 2,
+            count: 0
+        },
+        {
+            name: '1p',
+            value: 1,
+            count: 0
+        }
+    ];
+
+    /**
+     * Orders the moniesSizes from the higher to lower in value
+     */
+    function orderSizes() {
+        moniesSizes.sort(function (a, b) {
+            if (a.value < b.value) {
+                return 1;
+            }
+            if (a.value > b.value) {
+                return -1;
+            }
+            // a and b values are equal
+            return 0;
+        });
+    }
+
+    /**
+     * The input should already be formatted as float with max two decimals
+     *
+     * @param pennies
+     */
+    function convert(pennies) {
+        var size, coin,
+            countedCoins = [];
+
+        orderSizes();
+
+        for (size in moniesSizes) {
+            coin = {};
+            if (pennies / moniesSizes[size].value >= 1) {
+                coin.name = moniesSizes[size].name;
+                coin.value = moniesSizes[size].value;
+                coin.count = parseInt(pennies / moniesSizes[size].value);
+                countedCoins.push(coin);
+                // remainder
+                pennies -= (coin.value * coin.count);
+            }
+        }
+
+        return countedCoins;
+    }
+
+    return {
+        convert: convert
+    }
+})();
