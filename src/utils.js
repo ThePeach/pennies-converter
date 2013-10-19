992d34165565676f34721c7616c59ccd22e44b3e
Matteo 'Peach' Pescarin <peach@smartart.it>
2013-10-19 14:20:57 +0100

    added namespace utility, with basic tests

---
+++ src/utils.js
@@ -0,0 +1,30 @@
+/**
+ * create default namespace
+ * @type {*|{}}
+ */
+var PENNIES = PENNIES || {};
+/**
+ * Takes a namespace ns and creates it if it doesn't exist yet.
+ *
+ * @param {string} ns the namespace
+ * @returns {*|{}}
+ */
+PENNIES.namespace = function (ns) {
+    var parts = ns.split('.'),
+        parent = PENNIES,
+        i;
+
+    // strip redundant leading global
+    if (parts[0] === 'PENNIES') {
+        parts = parts.slice(1);
+    }
+
+    for (i=0; i < parts.length; i += 1) {
+        // create a property if it doesn't exist
+        if (typeof parent[parts[i]] === 'undefined') {
+            parent[parts[i]] = {};
+        }
+        parent = parent[parts[i]];
+    }
+    return parent;
+}
