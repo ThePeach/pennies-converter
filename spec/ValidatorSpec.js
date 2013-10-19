1d8d7e9f2404f6adfd605c984d1da689d093c1a0
Matteo 'Peach' Pescarin <peach@smartart.it>
2013-10-19 17:51:37 +0100

    implemented validator
    - updated input values on tests
    - created interface with the needed exported symbols

--- spec/ValidatorSpec.js
+++ spec/ValidatorSpec.js
@@ -5,11 +5,11 @@ describe('Validator', function () {
     var Validator = PENNIES.Validator;
 
     it('should keep digits intact', function () {
-        var values = [4, 25, 138, 1234],
+        var values = [ '4', '25', '138', '1234' ],
             i;
 
         for (i in values) {
-            expect(Validator.validate(values[i])).toEqual(values[i]);
+            expect(Validator.validate(values[i])).toEqual(parseInt(values[i]));
         }
     });
 
@@ -23,7 +23,7 @@ describe('Validator', function () {
     });
 
     it('should remove decimal point from digits', function () {
-        var values   = [ 1.0, 1.00, 1.2, 1.23 ],
+        var values   = [ '1.0', '1.00', '1.2', '1.23' ],
             expected = [ 100,  100, 120,  123 ],
             i;
 
@@ -33,7 +33,7 @@ describe('Validator', function () {
     });
 
     it ('should convert pounds into pennies', function () {
-        var values   = [ '£1', '£1.0', '£1.10', '£10' ],
+        var values   = [ '\xA31', '\xA31.0', '\xA31.10', '\xA310' ],
             expected = [  100,    100,     110,  1000 ],
             i;
 
@@ -43,7 +43,7 @@ describe('Validator', function () {
     });
 
     it ('should interpret pounds and pennies symbols', function () {
-        var values   = [ '£1p', '£1.p', '£1.10p', '£1.34p' ],
+        var values   = [ '\xA31p', '\xA31.p', '\xA31.10p', '\xA31.34p' ],
             expected = [  100,     100,      110,      134 ],
             i;
 
@@ -53,7 +53,7 @@ describe('Validator', function () {
     });
 
     it ('should remove leading zeroes', function () {
-        var values   = [ 0010, '010p', '£1.10p', '£1.34p' ],
+        var values   = [ '0010', '010p', '\xA31.10p', '\xA31.34p' ],
             expected = [   10,     10,      110,      134 ],
             i;
 
@@ -63,7 +63,7 @@ describe('Validator', function () {
     });
 
     it ('should round to two decimal places', function () {
-        var values   = [ 0.235, '0.125p', '1.125p', '£1.34512345p' ],
+        var values   = [ '0.235', '0.125p', '1.125p', '\xA31.34512345p' ],
             expected = [    24,       13,      113,            135 ],
             i;
 
@@ -73,7 +73,7 @@ describe('Validator', function () {
     });
 
     it('should discard non-valid input', function () {
-        var values = [ null, 0, -1, '', 'x', 'x8', '£1x.4p', '£p', '£-1.3', '£1,3p' ].
+        var values = [ null, false, '0', '-1', '', 'x', 'x8', '\xA31x.4p', '\xA3p', '\xA3-1.3', '\xA31,3p' ].
             i;
 
         for (i in values) {
