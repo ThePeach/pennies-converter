a91e9cf4c8d5c2df6eab2d18dc22c116280bcd8b
Matteo 'Peach' Pescarin <peach@smartart.it>
2013-10-20 10:54:10 +0100

    added global hinting and jsHint config

--- spec/NamespaceSpec.js
+++ spec/NamespaceSpec.js
@@ -1,7 +1,10 @@
+/*jshint bitwise:true, curly:true, eqeqeq:true, forin:true, noarg:true, noempty:true, nonew:true, undef:true, strict:true, browser:true */
+/*global PENNIES, describe, it, expect */
 /**
  * Basic set of tests for the namespace utils
  */
 describe('Namespace', function () {
+    'use strict';
 
     it('defines the property if not previously declared', function () {
         PENNIES.namespace('test');
