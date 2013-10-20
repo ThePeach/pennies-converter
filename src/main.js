85c54f059d6ad5027dc14046f939bef816b31974
Matteo 'Peach' Pescarin <peach@smartart.it>
2013-10-20 12:53:04 +0100

    refactored main.js and index.html according to latest changes
    - added jshint configuration comments
    - added use strict
    - added try/catch statements where needed
    - improved overall submit function

--- src/main.js
+++ src/main.js
@@ -1,38 +1,54 @@
+/*jshint bitwise:true, curly:true, eqeqeq:true, forin:true, noarg:true, noempty:true, nonew:true, undef:true, strict:true, browser:true */
+/*global PENNIES, jQuery */
 /**
  * @requires jQuery 2+
  *
  * This is the main controller for the JS application.
  * Handles the DOM and it's manipulation
  */
-
 (function ($) {
+    'use strict';
+
     var Converter = PENNIES.Converter,
-        Validator = PENNIES.Validator,
+        Validator = new PENNIES.utils.Validator(),
         $outputBox = $('.result');
 
     $('#pennies-converter').submit(function (e) {
         var pennies,
             monies,
             $resultStack = $('<ul></ul>'),
-            i;
+            i, error;
 
         e.preventDefault();
 
-        pennies = Validator.validate(this.pennies.value);
+        try {
+            pennies = Validator.validate(this.pennies.value);
+        } catch (error) {
+            $outputBox.html('<p>'+error.message+'</p>');
+            return false;
+        }
 
         if (Validator.hasErrors()) {
+            $outputBox.empty();
+
             for (i=0; i<Validator.messages.length; i+=1) {
-                $outputBox.html('<p>'+Validator.messages[i]+'</p>');
+                $outputBox.append('<p>'+Validator.messages[i]+'</p>');
             }
             return false;
         }
 
-        monies = Converter.convert(pennies);
+        try {
+            monies = Converter.convert(pennies);
+        } catch (error) {
+            $outputBox.html('<p>'+error.message+'</p>');
+            return false;
+        }
 
         for (i in monies) {
             $resultStack.append('<li>' + monies[i].name + ': x' + monies[i].count + '</li>');
         }
 
         $outputBox.empty().append($resultStack);
+        return true;
     });
 })(jQuery);
